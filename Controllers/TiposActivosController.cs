402194054f448ddd5cff0e87ee1fe9d84716f784
Aneudys <anortiz@apap.com.do>
2020-03-12 14:16:04 -0400

    Initial

---
+++ Controllers/TiposActivosController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ActivosFijosAPI;
+using ActivosFijosAPI.Models;
+
+namespace ActivosFijosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiposActivosController : ControllerBase
+    {
+        private readonly ActivosFijosDb _context;
+
+        public TiposActivosController(ActivosFijosDb context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TiposActivos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TipoActivo>>> GetTiposActivos()
+        {
+            return await _context.TiposActivos.ToListAsync();
+        }
+
+        // GET: api/TiposActivos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TipoActivo>> GetTipoActivo(int id)
+        {
+            var tipoActivo = await _context.TiposActivos.FindAsync(id);
+
+            if (tipoActivo == null)
+            {
+                return NotFound();
+            }
+
+            return tipoActivo;
+        }
+
+        // PUT: api/TiposActivos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTipoActivo(int id, TipoActivo tipoActivo)
+        {
+            if (id != tipoActivo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tipoActivo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TipoActivoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TiposActivos
+        [HttpPost]
+        public async Task<ActionResult<TipoActivo>> PostTipoActivo(TipoActivo tipoActivo)
+        {
+            _context.TiposActivos.Add(tipoActivo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTipoActivo", new { id = tipoActivo.Id }, tipoActivo);
+        }
+
+        // DELETE: api/TiposActivos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TipoActivo>> DeleteTipoActivo(int id)
+        {
+            var tipoActivo = await _context.TiposActivos.FindAsync(id);
+            if (tipoActivo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TiposActivos.Remove(tipoActivo);
+            await _context.SaveChangesAsync();
+
+            return tipoActivo;
+        }
+
+        private bool TipoActivoExists(int id)
+        {
+            return _context.TiposActivos.Any(e => e.Id == id);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,10 @@ namespace ActivosFijosAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddDbContext<ActivosFijosDb>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            //services.AddDbContext<ActivosFijosDb>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            var connection = "Data Source=./ActivosFijosDb.db";
+            services.AddDbContext<ActivosFijosDb>
+               (options => options.UseSqlite(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
